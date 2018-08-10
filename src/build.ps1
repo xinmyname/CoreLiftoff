8c622d78a0043178f759e1e69d172e5134f5ce4c
Andy Sherwood <sherwooda@cleanwaterservices.org>
2018-08-10 13:49:34 -0700

    Remove pesky old nupkg files for clean build

--- src/build.ps1
+++ src/build.ps1
@@ -1,5 +1,7 @@
 ﻿if ((Test-Path .\build)) { Remove-AllChildItems -r -fo .\build }
 New-Item -ItemType Directory .\build | Out-Null
+dotnet clean -c release
+gci -r -fi *.nupkg |% { $_.Delete() }
 dotnet build -c release
 Copy-Item -r .\Liftoff.Config\bin\Release\*.nupkg .\build
 Copy-Item -r .\Liftoff.Logging\bin\Release\*.nupkg .\build
